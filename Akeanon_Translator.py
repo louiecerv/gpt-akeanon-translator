9cee7083ff31c17e8c1b4192df294a185c14ee69
Louie Cervantes <louiecervantes@wvsu.edu.ph>
2024-04-29 13:49:17 +0800

    Save

---
+++ Akeanon_Translator.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import openai
+
+
+from openai import AsyncOpenAI
+from openai import OpenAI
+
+client = AsyncOpenAI(
+    # This is the default and can be omitted
+    api_key=st.secrets["API_key"],
+)
+
+context = "You are a language assistant"
+
+async def generate_response(question, context):
+    model = "ft:gpt-3.5-turbo-1106:west-visayas-state-university::9JDQf0yI"
+    
+    completion = await client.chat.completions.create(model=model, 
+        messages=[{"role": "user", "content": question}, 
+                {"role": "system", "content": context}])
+    return completion.choices[0].message.content
+
+async def app():
+    st.subheader("Akeanon-English / English Akeanon Translator")
+
+    # Define the options for the show selection
+    show_options = ["English to Akeanon", "Akeanon to English"]
+
+    # Use st.selectbox to create the show option box
+    selected_show = st.selectbox("Select the Task", show_options)
+
+    # Process the selected option
+    if selected_show:  # Check if user selected something
+        if selected_show == "English to Akeanon":
+            task = "translate to akeanon:"
+        elif selected_show == "Option 2":
+            task = "translate to enlish:"
+
+    # Text input for user question
+    question = st.text_input("Enter the sentence to translate:")
+    prompt = task + " " + question
+    # Button to generate response
+    if st.button("Translate"):
+        if question and context:
+            response = await generate_response(prompt, context)
+            st.write("Response:")
+            st.write(response)
+        else:
+            st.error("Please enter both question and context.")
+
+#run the app
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(app())
